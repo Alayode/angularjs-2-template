46e5270cfda44798a6d0415217f72259e1bb599d
Chris Samuel <ksamuel.chris@icloud.com>
2016-08-08 23:57:05 -0400

    created a mock-xhr backend and updated the providers

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,22 +1,25 @@
 //Validation Built in chapter 04-04 is added
 // Chapter 05 building a year Validators 
 // using FormBuilder private keyword and no longer need ControlGroup
+//05-06 importing Inject decorator to help support passing in a string literal
 
-import {Component} from 'angular2/core';
+import {Component,Inject} from 'angular2/core';
 import {Control,Validators,FormBuilder} from 'angular2/common';
-
+//creating a custom service
+import {MediaItemService} from './media-item.service'; 
 
 @Component({
     selector: 'media-item-form',
     templateUrl: 'app/media-item-form.component.html',
     styleUrls: ['app/media-item-form.component.css']
 })
+
 export class MediaItemFormComponent {
    form;
 
-   constructor(private formBuilder:FormBuilder){
-
-   }
+   constructor(private formBuilder:FormBuilder,
+    private mediaItemService: MediaItemService,
+    @Inject('LOOKUP_LISTS') public lookupLists) {}
 
    ngOnInit(){
        this.form = this.formBuilder.group({
---
+++ app/providers.ts
@@ -0,0 +1,8 @@
+import { OpaqueToken } from 'angular2/core';
+
+export var lookupLists = {
+    mediums: ['Movies', 'Turkey']
+};
+
+export var LOOKUP_LISTS = new OpaqueToken('LookupLists');
+
